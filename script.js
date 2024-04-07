a58e2e40ac44ed9791b4d39c040d2c30c03e4093
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-07 11:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const number1 = prompt("Enter the first number:");
+const number2 = prompt("Enter the second number:");
+
+const num1 = parseFloat(number1);
+const num2 = parseFloat(number2);
+
+const result = num1 === num2;
+
+aleart('Are the numbers equal? ${result}');
+
